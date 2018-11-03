86e6458ef85663a707c3286f29c068f29de1c02a
adiltirur <adiltirur@gmail.com>
2018-11-03 22:59:11 +0100

    increased fps

--- camera.js
+++ camera.js
@@ -1,19 +1,4 @@
-/**
- * @license
- * Copyright 2018 Google Inc. All Rights Reserved.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licnses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * =============================================================================
- */
+
 // import dat from 'dat.gui';
 // import Stats from 'stats.js';
 // import * as posenet from '../src';
@@ -186,6 +171,7 @@ function detectPoseInRealTime(video, net) {
 
         //minPoseConfidence = Number(guiState.multiPoseDetection.minPoseConfidence);
         //minPartConfidence = Number(guiState.multiPoseDetection.minPartConfidence);
+      //  minPartConfidence = Number(0.2):
         minPoseConfidence = Number(0.1);
         minPartConfidence = Number(0.5);
         break;
